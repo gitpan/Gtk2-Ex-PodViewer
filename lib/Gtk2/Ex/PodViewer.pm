d99d5e420cb691f455bc1b86a8709c27173388ff
Gavin Brown <gbrown@cpan.org>
2006-05-16 16:04:12 +0000

    Import of GBROWN/Gtk2-Ex-PodViewer-0.15 from CPAN.
    gitpan-cpan-distribution: Gtk2-Ex-PodViewer
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         GBROWN/Gtk2-Ex-PodViewer-0.15.tar.gz
    gitpan-cpan-author:       GBROWN
    gitpan-cpan-maturity:     released

--- lib/Gtk2/Ex/PodViewer.pm
+++ lib/Gtk2/Ex/PodViewer.pm
@@ -1,4 +1,4 @@
-# $Id: PodViewer.pm,v 1.40 2005/11/11 15:30:48 jodrell Exp $
+# $Id: PodViewer.pm,v 1.41 2006/05/16 16:01:19 gavin Exp $
 # Copyright (c) 2003-2005 Gavin Brown. All rights reserved. This program is
 # free software; you can redistribute it and/or modify it under the same
 # terms as Perl itself. 
@@ -10,7 +10,7 @@ use Gtk2::Pango;
 use Pod::Simple::Search;
 use strict;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 #
 # we want to create a new signal for this object, which means we need to
